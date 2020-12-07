45b00fc661798bb3ea13db6fb8eabf40f8d27b61
emilyjmaloney <emilyjean.maloney@gmail.com>
2020-12-07 01:03:42 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,53 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var suits = [
+    { suit: "♣", color: "black" },
+    { suit: "❤️", color: "red" },
+    { suit: "♠", color: "black" },
+    { suit: "♦", color: "red" }
+  ];
+
+  var randomValue = values[Math.floor(Math.random() * values.length)];
+  var randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  var card = document.createElement("div");
+
+  var valueMiddle = document.createElement("div");
+  var suitUpper = document.createElement("div");
+  var suitLower = document.createElement("div");
+
+  card.classList.add("card");
+
+  suitUpper.classList.add("suit");
+  suitLower.classList.add("bsuit");
+  valueMiddle.classList.add("value");
+
+  valueMiddle.innerHTML = randomValue;
+  suitUpper.innerHTML = randomSuit.suit;
+  suitUpper.style.color = randomSuit.color;
+  suitLower.innerHTML = randomSuit.suit;
+  suitLower.style.color = randomSuit.color;
+  valueMiddle.style.color = randomSuit.color;
+
+  card.appendChild(suitUpper);
+  card.appendChild(valueMiddle);
+  card.appendChild(suitLower);
+
+  document.body.appendChild(card);
 };
